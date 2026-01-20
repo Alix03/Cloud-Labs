509d9ecdc73981da62df0fed40c16dc9e9ed66c1
Alice Boccadifuoco <139858047+Alix03@users.noreply.github.com>
2026-01-20 03:07:50 +0100

    Refactor calculate_integral by removing comments
    Removed unnecessary comments in calculate_integral function for clarity.

--- 4_function/function_app.py
+++ 4_function/function_app.py
@@ -7,16 +7,13 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.route(route="calculate_integral", methods=["GET"])
 def calculate_integral(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        # Recupero parametri dalla URL
         a = float(req.params.get('a'))
         b = float(req.params.get('b'))
-        n = int(req.params.get('n', 10000)) # Default a 10.000 se manca
+        n = int(req.params.get('n', 10000)) 
 
-        # Logica del calcolo dell'integrale (|sin(x)|)
         h = (b - a) / n
         def f(x): return abs(math.sin(x))
         
-        # Regola del trapezio
         result = 0.5 * (f(a) + f(b))
         for i in range(1, n):
             result += f(a + i * h)
